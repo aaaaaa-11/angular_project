301246c956f8d1847acfa43fb74668fc85778d99
aaaaaa-11 <2823901437@qq.com>
2020-05-28 11:50:40 +0800

    初始化

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,49 +3,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // 引入组件
-import { IndexComponent } from './components/index/index.component';
-import { ProductComponent } from './components/product/product.component'
-import { NewsComponent } from './components/news/news.component'
-import { NewscontentComponent } from './components/newscontent/newscontent.component';
-import { ProductcontentComponent } from './components/productcontent/productcontent.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
-import { WelcomeComponent } from './components/homepage/welcome/welcome.component';
+
 
 const routes: Routes = [
-  {
-    path: 'index',
-    component: IndexComponent
-  },
-  {
-    path: 'news',
-    component: NewsComponent
-  },
-  {
-    path: 'newscontent/:aid',
-    component: NewscontentComponent
-  },
-  {
-    path: 'product',
-    component: ProductComponent
-  },
-  {
-    path: 'productcontent/:pid',
-    component: ProductcontentComponent
-  },
-  {
-    path: '**',
-    redirectTo: 'home'
-  },
-  {
-    path: 'homepage',
-    component: HomepageComponent,
-    children: [
-    ]
-  },
-  {
-    path: 'welcome',
-    component: WelcomeComponent
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,34 +11,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http'
 
 // 引入并配置服务
-import { RequestService } from './services/request.service';
-import { HttpserviceService } from './services/httpservice.service';
-
 
 // 根组件
 import { AppComponent } from './app.component';
 
-import { NewsComponent } from './components/news/news.component';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FormComponent } from './components/form/form.component';
-import { TodoComponent } from './components/todo/todo.component';
-import { SearchComponent } from './components/search/search.component';
-
-import { DomComponent } from './components/dom/dom.component';
-import { TransitionComponent } from './components/transition/transition.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ParentComponent } from './components/parent/parent.component';
-import { LifecircleComponent } from './components/lifecircle/lifecircle.component';
-import { RequestComponent } from './components/request/request.component';
-import { AxiosComponent } from './components/axios/axios.component';
-import { ProductComponent } from './components/product/product.component';
-import { WelcomeComponent } from './components/homepage/welcome/welcome.component';
 
 // @NgModule装饰器，@NgModule接受一个元数据对象，告诉Angular如何编译和启动应用
 @NgModule({
   declarations: [ /* 配置当前项目运行的组件 */
-    AppComponent, NewsComponent, HomeComponent, HeaderComponent, FormComponent, TodoComponent, SearchComponent, DomComponent, TransitionComponent, FooterComponent, ParentComponent, LifecircleComponent, RequestComponent, AxiosComponent, ProductComponent, WelcomeComponent
+    AppComponent
   ],
   imports: [ /* 配置当前模块运行依赖的其他模块 */
     BrowserModule,
@@ -47,7 +28,7 @@ import { WelcomeComponent } from './components/homepage/welcome/welcome.componen
     HttpClientModule,
     HttpClientJsonpModule
   ],
-  providers: [RequestService, HttpserviceService], /* 配置项目所需的服务 */
+  providers: [], /* 配置项目所需的服务 */
   bootstrap: [AppComponent] /* 指定应用的主视图 */
 })
 // 根模块不需要导出任何东西，因为其他组件不需要导入根模块
